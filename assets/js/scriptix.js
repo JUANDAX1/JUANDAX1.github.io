e2b4d164937b59e80739d8824c3914d07bac7b02
JUANDAX1 <58204869+JUANDAX1@users.noreply.github.com>
2023-11-14 22:07:28 -0300

    comit actualizacion

--- assets/js/scriptix.js
+++ assets/js/scriptix.js
@@ -1,6 +1,6 @@
 // scriptix.js
 
-
+/*
 const imagenesExpansibles = document.querySelectorAll(".expandirx");
 
 imagenesExpansibles.forEach(function (imagen) {
@@ -10,15 +10,36 @@ imagenesExpansibles.forEach(function (imagen) {
             imagen.style.transformOrigin = "center center";
             imagen.classList.remove("expandida");
         } else {
-            imagen.style.transform = "scale(2)";
+            imagen.style.transform = "scale(1.5)";
             imagen.style.transformOrigin = "center center";
             imagen.classList.add("expandida");
         }
     });
 });
+*/
+
+const imagenesExpansibles = document.querySelectorAll(".expandirx");
+
+imagenesExpansibles.forEach(function (imagen) {
+    imagen.addEventListener("click", function () {
+        if (imagen.classList.contains("expandida")) {
+            imagen.style.transform = "scale(1)";
+            imagen.style.transformOrigin = "center center";
+            imagen.classList.remove("expandida");
+            imagen.style.zIndex = "auto"; // Restablecer el z-index a su valor predeterminado
+        } else {
+            // Asignar un z-index más alto cuando la imagen se expanda
+            imagen.style.transform = "scale(1.5)";
+            imagen.style.transformOrigin = "center center";
+            imagen.classList.add("expandida");
+            imagen.style.zIndex = "9999"; // Establecer un valor alto de z-index
+        }
+    });
+});
+
 
 function confirmarDescarga() {
-    var confirmacion = confirm("¿deseas descargar el archivo Busines Case?");
+    var confirmacion = confirm("¿deseas descargar el archivo Business Case?");
     if (confirmacion) {
         window.location.href = "images/Business Case_Ecommerce_Public.xlsx";
     }
